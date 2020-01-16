3ff96601c47907d0e753a26361af00d707d0769d
TerZhang <terzhang@hotmail.com>
2020-01-16 05:54:15 -0500

    Add CSSReset and Provider as instructed by Chakra

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Providers from './Providers';
 import Auth from './pages/Auth';
+import { CSSReset } from '@chakra-ui/core';
 
 function App() {
   return (
     <Providers>
+      <CSSReset />
       <Auth />
     </Providers>
   );
--- src/Providers.js
+++ src/Providers.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Provider as PlayerProvider } from './contexts/players';
 import { Provider as RoundProvider } from './contexts/round';
 import { Provider as GameProvider } from './contexts/game';
+import { ThemeProvider } from '@chakra-ui/core';
+import theme from './theme';
 
 // HOF provider provider
 const Providers = ({ children }) => (
   <GameProvider>
     <RoundProvider>
-      <PlayerProvider>{children}</PlayerProvider>
+      <PlayerProvider>
+        <ThemeProvider>{children}</ThemeProvider>
+      </PlayerProvider>
     </RoundProvider>
   </GameProvider>
 );
