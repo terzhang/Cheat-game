cd898a1ced303383d14e6db35a330078de48b6e2
TerZhang <terzhang@hotmail.com>
2020-01-18 00:17:39 -0500

    Add online button for the future on Room page

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { navigate } from 'hookrouter';
 import useStartOffline from '../hooks/useStartOffline';
-import { Flex, Button } from '@chakra-ui/core';
+import { Flex, Button, ButtonGroup, CircularProgress } from '@chakra-ui/core';
 
 const Room = () => {
+  const [loading, setLoading] = useState(false);
   const startOffline = useStartOffline();
 
-  const handleStart = () => {
+  const handleOfflineStart = () => {
+    setLoading(true);
     // generate a hand for each player
     startOffline();
-    navigate('/game');
-    // decide who is the starting dealer
-    // decide a starting card
-    // display round actions
+    setLoading(false);
+    // pass the isOnline param to next page
+    navigate('/game', false, { isOnline: false });
+  };
+
+  const handleOnlineStart = () => {
+    setLoading(true);
+    //startOnline(id)
+    setLoading(false);
+    navigate('/game', false, { isOnline: true });
   };
 
   return (
     <Flex as='main' justify='center' align='center'>
-      <Button onClick={handleStart}>Start Game!</Button>
+      {loading && <CircularProgress isIndeterminate />}
+      <ButtonGroup spacing={4}>
+        <Button onClick={handleOfflineStart} isLoading={loading}>
+          Start Offline
+        </Button>
+        <Button isDisabled onClick={handleOnlineStart} isLoading={loading}>
+          Play Online (not yet available)
+        </Button>
+      </ButtonGroup>
     </Flex>
   );
 };
