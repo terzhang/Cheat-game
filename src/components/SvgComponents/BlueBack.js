ff34a46072867fc9e3e939d1fc1c38d09783117f
TerZhang <terzhang@hotmail.com>
2020-01-18 00:24:18 -0500

    Add in the svgr converted svg components

---
+++ src/components/SvgComponents/BlueBack.js
@@ -0,0 +1,135 @@
+import React from "react";
+
+const SvgBlueBack = props => (
+  <svg width={223.228} height={311.811} {...props}>
+    <defs>
+      <pattern
+        patternTransform="rotate(135.001 -142.425 -15.397) scale(.15641)"
+        id="Blue_Back_svg__j"
+        xlinkHref="#Blue_Back_svg__a"
+      />
+      <pattern
+        id="Blue_Back_svg__a"
+        patternTransform="translate(-254.024 74.426)"
+        height={110.741}
+        width={108.618}
+        patternUnits="userSpaceOnUse"
+      >
+        <path
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          transform="translate(1070.355 -11.764)"
+          fill="#131f67"
+        />
+        <path
+          transform="rotate(30 -588.091 2050.257)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#131f67"
+        />
+        <path
+          transform="translate(1124.664 -11.764)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#131f67"
+        />
+        <path
+          transform="rotate(30 -560.936 2151.6)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#131f67"
+        />
+      </pattern>
+      <pattern
+        patternTransform="scale(.15023) rotate(-45 922.96 6085.23)"
+        id="Blue_Back_svg__b"
+        xlinkHref="#Blue_Back_svg__c"
+      />
+      <pattern
+        patternTransform="scale(.15023) rotate(-45 -2473.244 6852.978)"
+        id="Blue_Back_svg__c"
+        xlinkHref="#Blue_Back_svg__d"
+      />
+      <pattern
+        patternTransform="scale(.15023) rotate(-45 227.657 -29.145)"
+        id="Blue_Back_svg__d"
+        xlinkHref="#Blue_Back_svg__e"
+      />
+      <pattern
+        patternTransform="scale(.15023) rotate(-45 730.249 525.163)"
+        id="Blue_Back_svg__e"
+        xlinkHref="#Blue_Back_svg__f"
+      />
+      <pattern
+        patternTransform="scale(.15023) rotate(-45 47.044 -1811.87)"
+        id="Blue_Back_svg__f"
+        xlinkHref="#Blue_Back_svg__g"
+      />
+      <pattern
+        patternTransform="matrix(.0652 -.0652 .0652 .0652 194.547 -74.73)"
+        id="Blue_Back_svg__g"
+        xlinkHref="#Blue_Back_svg__h"
+      />
+      <pattern
+        patternTransform="scale(.15565) rotate(-45 -238.81 2209.061)"
+        id="Blue_Back_svg__h"
+        xlinkHref="#Blue_Back_svg__i"
+      />
+      <pattern
+        id="Blue_Back_svg__i"
+        patternTransform="translate(-254.024 74.426)"
+        height={110.741}
+        width={108.618}
+        patternUnits="userSpaceOnUse"
+      >
+        <path
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          transform="translate(1070.355 -11.764)"
+          fill="#d40000"
+        />
+        <path
+          transform="rotate(30 -588.091 2050.257)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#d40000"
+        />
+        <path
+          transform="translate(1124.664 -11.764)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#d40000"
+        />
+        <path
+          transform="rotate(30 -560.936 2151.6)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#d40000"
+        />
+      </pattern>
+    </defs>
+    <g transform="translate(259.614 -501.316)">
+      <rect
+        width={222.228}
+        height={310.811}
+        x={-259.114}
+        y={501.816}
+        ry={10.63}
+        rx={10.63}
+        fill="#fff"
+        stroke="#000"
+      />
+      <rect
+        width={197.052}
+        height={286.644}
+        x={-246.526}
+        y={513.9}
+        ry={0}
+        fill="none"
+        stroke="#131f67"
+        strokeWidth={4.952}
+        strokeLinecap="round"
+      />
+      <path
+        fill="url(#Blue_Back_svg__j)"
+        stroke="#131f67"
+        strokeLinecap="round"
+        d="M-242.139 517.769h188.277v278.906h-188.277z"
+      />
+    </g>
+  </svg>
+);
+
+export default SvgBlueBack;
---
+++ src/components/SvgComponents/RedBack.js
@@ -0,0 +1,161 @@
+import React from "react";
+
+const SvgRedBack = props => (
+  <svg width={223.228} height={311.811} {...props}>
+    <defs>
+      <pattern
+        patternTransform="rotate(-45 711.917 114.198)"
+        id="Red_Back_svg__k"
+        xlinkHref="#Red_Back_svg__a"
+      />
+      <pattern
+        id="Red_Back_svg__b"
+        patternTransform="translate(-254.024 74.426)"
+        height={110.741}
+        width={108.618}
+        patternUnits="userSpaceOnUse"
+      >
+        <path
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          transform="translate(1070.355 -11.764)"
+          fill="#131f67"
+        />
+        <path
+          transform="rotate(30 -588.091 2050.257)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#131f67"
+        />
+        <path
+          transform="translate(1124.664 -11.764)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#131f67"
+        />
+        <path
+          transform="rotate(30 -560.936 2151.6)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#131f67"
+        />
+      </pattern>
+      <pattern
+        patternTransform="scale(.15023) rotate(-45 922.96 6085.23)"
+        id="Red_Back_svg__c"
+        xlinkHref="#Red_Back_svg__d"
+      />
+      <pattern
+        patternTransform="scale(.15023) rotate(-45 -2473.244 6852.978)"
+        id="Red_Back_svg__d"
+        xlinkHref="#Red_Back_svg__e"
+      />
+      <pattern
+        patternTransform="scale(.15023) rotate(-45 227.657 -29.145)"
+        id="Red_Back_svg__e"
+        xlinkHref="#Red_Back_svg__f"
+      />
+      <pattern
+        patternTransform="scale(.15023) rotate(-45 730.249 525.163)"
+        id="Red_Back_svg__f"
+        xlinkHref="#Red_Back_svg__g"
+      />
+      <pattern
+        patternTransform="scale(.15023) rotate(-45 47.044 -1811.87)"
+        id="Red_Back_svg__g"
+        xlinkHref="#Red_Back_svg__h"
+      />
+      <pattern
+        patternTransform="matrix(.0652 -.0652 .0652 .0652 194.547 -74.73)"
+        id="Red_Back_svg__h"
+        xlinkHref="#Red_Back_svg__i"
+      />
+      <pattern
+        patternTransform="scale(.15565) rotate(-45 -238.81 2209.061)"
+        id="Red_Back_svg__i"
+        xlinkHref="#Red_Back_svg__j"
+      />
+      <pattern
+        id="Red_Back_svg__j"
+        patternTransform="translate(-254.024 74.426)"
+        height={110.741}
+        width={108.618}
+        patternUnits="userSpaceOnUse"
+      >
+        <path
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          transform="translate(1070.355 -11.764)"
+          fill="#d40000"
+        />
+        <path
+          transform="rotate(30 -588.091 2050.257)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#d40000"
+        />
+        <path
+          transform="translate(1124.664 -11.764)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#d40000"
+        />
+        <path
+          transform="rotate(30 -560.936 2151.6)"
+          d="M-1016.046 25.72l-10.764 16.121 7.177 18.006-18.658-5.255-14.907 12.39-.767-19.369-16.39-10.349 18.184-6.714 4.777-18.786 12.005 15.218z"
+          fill="#d40000"
+        />
+      </pattern>
+      <pattern
+        id="Red_Back_svg__a"
+        patternTransform="translate(127.765 536.85)"
+        height={17.116}
+        width={16.772}
+        patternUnits="userSpaceOnUse"
+      >
+        <g fill="#d40000">
+          <path
+            transform="matrix(.62549 0 0 .62549 -79.915 -335.792)"
+            d="M139.931 538.17l-1.53 4.514 2.85 3.82-4.766-.06-2.753 3.891-1.415-4.552-4.552-1.415 3.892-2.752-.06-4.767 3.82 2.851z"
+          />
+          <path
+            d="M139.931 538.17l-1.53 4.514 2.85 3.82-4.766-.06-2.753 3.891-1.415-4.552-4.552-1.415 3.892-2.752-.06-4.767 3.82 2.851z"
+            transform="scale(.62549) rotate(60 542.293 175.25)"
+          />
+          <path
+            d="M139.931 538.17l-1.53 4.514 2.85 3.82-4.766-.06-2.753 3.891-1.415-4.552-4.552-1.415 3.892-2.752-.06-4.767 3.82 2.851z"
+            transform="matrix(.62549 0 0 -.62549 -79.915 352.908)"
+          />
+          <path
+            transform="scale(.62549 -.62549) rotate(60 565.99 161.567)"
+            d="M139.931 538.17l-1.53 4.514 2.85 3.82-4.766-.06-2.753 3.891-1.415-4.552-4.552-1.415 3.892-2.752-.06-4.767 3.82 2.851z"
+          />
+        </g>
+      </pattern>
+    </defs>
+    <g transform="translate(550.942 -490.002)">
+      <rect
+        rx={10.63}
+        ry={10.63}
+        y={490.502}
+        x={-550.442}
+        height={310.811}
+        width={222.228}
+        fill="#fff"
+        stroke="#000"
+      />
+      <rect
+        ry={0}
+        y={502.586}
+        x={-537.854}
+        height={286.644}
+        width={197.052}
+        fill="none"
+        stroke="#d40000"
+        strokeWidth={4.952}
+        strokeLinecap="round"
+      />
+      <path
+        fill="url(#Red_Back_svg__k)"
+        stroke="#d40000"
+        strokeLinecap="round"
+        d="M-533.467 506.455h188.277v278.906h-188.277z"
+      />
+    </g>
+  </svg>
+);
+
+export default SvgRedBack;
