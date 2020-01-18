a1c3561c39b97eda4df8a1aca0da62f5d2b40ed5
TerZhang <terzhang@hotmail.com>
2020-01-18 00:21:00 -0500

    Add toast, background images, and url query

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,18 +1,27 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { Flex } from '@chakra-ui/core';
-import { navigate } from 'hookrouter';
+import React, { useState, useContext, useEffect, useCallback } from 'react';
+import { Flex, Button, useToast, Image } from '@chakra-ui/core';
+import { navigate, useQueryParams } from 'hookrouter';
 import Hand from '../components/Hand'; // components
 import { Context as gameContext } from '../contexts/game'; // contexts
 import { Context as playerContext } from '../contexts/players';
-
+import { Context as roundContext } from '../contexts/round';
+// hook
+import useNextRound from '../hooks/useNextRound';
 import CardSlider from '../components/CardSlider';
+// background and table
+import texture from '../assets/texture.jpg';
+import pokerTable from '../assets/poker-table.png';
 
 const Game = () => {
+  const [queryParams, setQueryParams] = useQueryParams();
   const [myHand, setMyHand] = useState(null);
+  const [cardToCall, setCardToCall] = useState(null);
   const {
     state: { self },
   } = useContext(gameContext);
-
+  const {
+    state: { dealer },
+  } = useContext(roundContext);
   const { state: players } = useContext(playerContext);
 
   const myself = React.useCallback(() => {
@@ -23,23 +32,96 @@ const Game = () => {
     }
   }, [players, self]);
 
+  const isDealer = useCallback(() => dealer === self, [dealer, self]);
+
+  const nextRound = useNextRound();
+
+  const toast = useToast();
+
+  // pick a dealer and start the new or next round
   useEffect(() => {
+    const online = queryParams.isOnline;
+    const toastConfig = {
+      position: 'top',
+      status: 'info',
+      duration: 2500,
+      isClosable: true,
+      title: '',
+      description: '',
+    };
+    // offline, then
+    if (!online) {
+      // show toast that you're playing offline.
+      toast({
+        ...toastConfig,
+        title: 'Offline',
+        description: 'Enjoy your time with our robot overlords ;)',
+      });
+    } else {
+      // show toast that you're playing online
+      toast({
+        ...toastConfig,
+        status: 'success',
+        title: 'Online!',
+        description: 'Enjoy your time with these lovely Card Cheaters',
+      });
+    }
+    // call nextRound() to pick a dealer
+    if (players.length >= 2) {
+      nextRound();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    // if name is valid, get own hand
     if (self) {
       const me = myself();
       const hand = me.hand;
       setMyHand(hand);
-    } else {
-      console.log('wrong route');
-      navigate('/');
     }
+    // if we are the dealer, show option
   }, [players, self, myself]);
 
+  const handleSliderChange = () => {};
+
+  const cardSelector = () => (
+    <>
+      <CardSlider currentCardNum={0} />
+      <Button borderRadius='md' onClick={() => {}}>
+        Call card as {cardToCall}
+      </Button>
+    </>
+  );
+
   return (
-    <Flex as='main' w='full' direction='column' justify='center' align='center'>
+    <Flex
+      as='main'
+      w='full'
+      direction='column'
+      justify='center'
+      align='center'
+      bgImage={`url(${texture})`}
+      bgPos='center'
+      bgRepeat='no-repeat'
+      bgSize='cover'
+      title='card table texture'
+    >
+      <Image
+        src={pokerTable}
+        bgPos='center'
+        bgRepeat='no-repeat'
+        position='absolute'
+        zIndex={0}
+        pointerEvents='none'
+        height='inherit'
+        width='inherit'
+        alt='poker table'
+      />
       {myHand && (
         <Hand handArray={myHand} wrapperStyle={{ justifyContent: 'center' }} />
       )}
-      <CardSlider currentCardNum={0} />
+      {isDealer() && cardSelector}
     </Flex>
   );
 };
